36328c59445daf42d8882c46f21a4b309f826db3
Edson Alves <edsonapf@gmail.com>
2025-03-18 12:20:40 -0300

    fix: api env var

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import { ExperimentData } from "@/interfaces/ExperimentData";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   headers: {
     "ngrok-skip-browser-warning": "true",
   },
